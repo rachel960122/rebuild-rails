4abfee0d014681936969d4b902afb27daf3f106d
Rachel Xu <rachel960122@hotmail.com>
2016-06-15 10:02:55 -0700

    Add test, routing and upgrade gem

--- rulers.gemspec
+++ rulers.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
 
   spec.add_runtime_dependency "rack"
+  spec.add_development_dependency "rack-test"
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+# rulers/test/test_helper.rb
+require "rack/test"
+require "test/unit"
+
+this_dir = File.join(File.dirname(__FILE__), "..")
+$LOAD_PATH.unshift File.expand_path(this_dir)
+
+require "rulers"
